49ae7c9b5b1f1a6c78a3a62336af6846775fafea
mahrus-rohisyam <mahrusaim@gmail.comm>
2024-02-18 19:43:27 +0700

    initial project

---
+++ app.js
@@ -0,0 +1,48 @@
+// const site = window.location.hostname;
+// const currency = document.getElementsByClassName("svelte-1d6bfct");
+// const buttonStanger = document.getElementById("button_stanger");
+alert("Hello world!");
+// document.addEventListener("DOMContentLoaded", function () {
+//   alert(
+//     "Hello world! I am ready to conquer the stake.com " + currency[0].innerHTML
+//   );
+
+//   var elements = document.getElementsByClassName("svelte-1d6bfct");
+//   Array.prototype.forEach.call(elements, function (element) {
+//     element.innerHTML = element.innerHTML.replace(/RUB/g, "$");
+//   });
+// });
+// alert("hi");
+// $(document).ready(function () {
+//   alert(
+//     "Hello world! I am ready to conquer the stake.com " + currency[0].innerHTML
+//   );
+//   $(".svelte-1d6bfct").each(function () {
+//     $(this).html($(this).html().replace(/RUB/g, "$"));
+//   });
+// });
+
+// function handleClick() {
+//   console.log("Button clicked!");
+// }
+
+// function checkAndAttachListener() {
+//   if ($(".svelte-1d6bfct").length > 0) {
+//     $(document).on("click", '[data-test="bet-button"]', function () {
+//       handleClick();
+//     });
+//   } else {
+//     setTimeout(checkAndAttachListener, 1000);
+//   }
+// }
+
+// $(document).ready(function () {
+//   checkAndAttachListener();
+// });
+
+// var currencyElement =
+//   document.getElementsByClassName("svelte-1d6bfct").elements[0].innerHTML;
+// console.log(currencyElement);
+// alert(currencyElement);
+
+// currencyElement = currencyElement.replace(/RUB/g, "$");
---
+++ popup.js
@@ -0,0 +1,6 @@
+// const currency = document.getElementsByClassName("svelte-1d6bfct");
+
+// function button_activate_click(e) {
+//   e.preventDefault();
+//   alert(currency[0].innerHTML);
+// }
